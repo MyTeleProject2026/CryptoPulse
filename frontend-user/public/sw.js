09083cb95251bde045eb2598a619273fee4d55da
unknown <samsmith00112211@gmail.com>
2026-04-17 18:34:01 -0400

    Fix: Mobile scroll bar issue and avatar crop for CryptoPulse

---
+++ frontend-user/public/sw.js
@@ -0,0 +1,46 @@
+// Service Worker for CryptoPulse PWA
+const CACHE_NAME = 'cryptopulse-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/cryptopulse-icon.svg',
+  '/cryptopulse-logo.svg'
+];
+
+// Install event - cache assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Fetch event - serve from cache then network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate event - clean old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
